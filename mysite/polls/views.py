a64605c7f2785c0b91e7a86b370e70b54233fdfa
Dima Bukatsel <dimasgrand16@gmail.com>
2022-06-02 14:13:00 +0300

    update views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 from django.views import generic
 
 from .models import Choice, Question
-# Create your views here.
+
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
@@ -14,11 +14,9 @@ class IndexView(generic.ListView):
         """Return the last five published questions."""
         return Question.objects.order_by('-pub_date')[:5]
 
-
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
-
 
 class ResultsView(generic.DetailView):
     model = Question
